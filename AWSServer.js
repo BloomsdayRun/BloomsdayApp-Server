191f7d5e4487450402a3ee11d84e4ea7239a3012
chauncyc <ccullitan@zagmail.gonzaga.edu>
2015-11-11 13:57:03 -0800

    Don't throw error; instead send message to client

--- AWSServer.js
+++ AWSServer.js
@@ -34,8 +34,9 @@ app.get( '/api/runner/', function(request, response) {
 
     connection.query(query, function(err, rows, fields) {
         if (err) {
-            response.send('error retrieving from database');
-            throw err; //TODO: Don't shutdown server on error
+            //TODO: Don't send error message to client in production
+            response.send("error retrieving from database: " + err);
+            // throw err; //throwing shuts down server
         } else {
             if (rows[0]) { //i.e., if the response is not null/undefined
                 console.log('Response: ', rows[0]);
@@ -56,14 +57,15 @@ app.post( '/api/runner/', function(request, response) {
     var longitude = request.query.longitude;
     var timestamp = request.query.timestamp;
     //TODO: There's probably a library for autoformatting SQL queries
+    //TODO: If this RunnerID already exists, alter, don't create a new row
     var query = 
         "INSERT INTO Runner (RunnerId, Latitude, Longitude, TimeStamp) VALUES ('" 
         + id + "', '" + latitude + "', '" + longitude + "', '" + timestamp + "');";
     console.log(query);
     connection.query(query, function(err, rows, fields) {
         if (err) {
-            response.send("error inserting into database");
-            throw err; //TODO: Don't shutdown server on error
+            response.send("error inserting into database: " + err);
+            // throw err; //TODO: Don't shutdown server on error
         } else {
             response.send("Insertion should be successful");
         }
