c191c923b94eca356c9b30501613dab79a7d6406
chauncyc <ccullitan@zagmail.gonzaga.edu>
2015-11-24 12:05:16 -0800

    Add unit test for POST

--- WebAPI/tests/RouteTests.js
+++ WebAPI/tests/RouteTests.js
@@ -1,7 +1,7 @@
 var request = require('supertest');
 // var path = require('path');
 
-describe("Route Tests :: ", function() {
+describe("Basic Route Tests :: ", function() {
     var server;
     beforeEach(function () {
         //Erase server from cache to ensure it is reloaded for each test
@@ -24,14 +24,25 @@ describe("Route Tests :: ", function() {
          .expect(404, done);
     });
 
+    var fakeId = 12345;
     //No Access Token
     //TODO: When error/success messages are determined, parse & verify response body
-    it("GET", function testGet(done) {
+    it("GET - No Access Token", function testGet(done) {
         request(server)
-         .get("/api/runner/?id=" + 12345)
+         .get("/api/runner/?id=" + fakeId)
+         // .get("/api/runner/")
+         // .field("id", fakeId)
+         // .send({id: fakeId})
          .expect(200, done);
     });
 
-    
+    //TODO: Insert URL parameters programatically
+    it("POST - No Access Token", function testPost(done) {
+        request(server)         
+         .post("/api/runner/?id=" + fakeId + "&latitude=20&longitude=30&timestamp=99999")
+         .expect(200, done);
+    });
+
+
 
 });
